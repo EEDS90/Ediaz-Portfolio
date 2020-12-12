a09d9035152f02261e57d9af8c86349b27e50c9d
eeds90 <eddie.diaz90@icloud.com>
2020-12-12 16:34:04 -0500

    started portfolio

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+// import react from react
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <p>
+         Hello Word!
+        </p>
+       
+    </div>
+  );
+}
+
+export default App;
