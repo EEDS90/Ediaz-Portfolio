716b66e662e48f4ab0d9a9fb4a030ece6bae3101
eeds90 <eddie.diaz90@icloud.com>
2020-12-13 16:43:13 -0500

    about me page 50% completed

--- client/src/screens/About.jsx
+++ client/src/screens/About.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
-import './About.css';
+import React from "react";
+import "./About.css";
 
 export default function About(props) {
-    return(
-        <>
-            <div>
-                <h1>About Me</h1>
-                <p>Blah blah blah!</p>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <h1>About Me</h1>
+        <p>
+          Technology professional proficient in finding solutions that drive
+          organizational processes and exceed customer expectations. Adaptable
+          to the ever-changing IT landscape and focused on providing solutions
+          that meet the needs of my team, the organization and the customer.
+          Bilingual professional with fluency in Spanish/English with strong
+          problem solving and analytical skills.
+        </p>
+        <div>
+          <h2>Languages, tech stacks and databases:</h2>
+          <p>
+            HTML5, CSS, JavaScript, React.js, Node.js, Ruby on Rails, SQL,
+            PostgreSQL, MongoDB, NoSQL, GitHub, Bootstrap, Git.
+          </p>
+        </div>
+        <div>
+          <h3>Resume</h3>
+          <p>
+            <a></a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
