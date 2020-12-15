458d21c906d7d946e3141ff52d20e958613444cb
eeds90 <eddie.diaz90@icloud.com>
2020-12-14 20:24:27 -0500

    added project1

--- client/src/screens/Work.jsx
+++ client/src/screens/Work.jsx
@@ -1,31 +1,39 @@
 import React from "react";
 import "./Work.css";
+import image1 from "../../src/assets/project1.png";
 
 export default function Work(props) {
   return (
     <>
-      <div>
-        <h1>Project 1</h1>
-        <p>
-          Technology professional proficient in finding solutions that drive
-          organizational processes and exceed customer expectations. Adaptable
-          to the ever-changing IT landscape and focused on providing solutions
-          that meet the needs of my team, the organization and the customer.
-          Bilingual professional with fluency in Spanish/English with strong
-          problem solving and analytical skills.
-        </p>
+      <div className="project1">
+        <img className="project-imgs" src={image1} alt="project png" />
+        <div className="card-content">
+          <h2 className="projects-name">Roten Potatoes</h2>
+          <p className="projects-description">
+            Rotten Potatoes This user friendly React app allows users to access
+            New Releases with a cool hover over overview feature and allows the
+            user to create their own reviews. It was created using conditional
+            rendering with mock API data.{" "}
+          </p>
+          <div className="project-buttons">
+            <a
+              href=" https://rotten-potatoes.netlify.app "
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="ls-buttons">Visit Site</button>
+            </a>
+            <a
+              href="https://github.com/EEDS90/Movie-App"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="gh-buttons">GitHub</button>
+            </a>
 
-        <div>
-          <h2>Project 2</h2>
-          <p>
-            <a></a>
-          </p>
-        </div>
-        <div>
-          <h3>Project 3</h3>
-          <p>
-            <a></a>
-          </p>
+
+            
+          </div>
         </div>
       </div>
     </>
