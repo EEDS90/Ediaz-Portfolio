1206c34dfced84ec92c03e03ec460fad1b42b824
eeds90 <eddie.diaz90@icloud.com>
2020-12-14 20:47:01 -0500

    added buttons to contact page

--- client/src/screens/Contact.jsx
+++ client/src/screens/Contact.jsx
@@ -7,8 +7,23 @@ export default function Contact(props) {
             <div>
                 <h1>Contact Info</h1>
                 <p>Email: ee.diaz.susana1@gmail.com </p>
-                <p>LinkedIn: https://www.linkedin.com/in/edwin-diaz-663b93194/detail/contact-info/ </p>
-                <p>Github: https://github.com/EEDS90 </p>
+                
+          <div className="project-buttons">
+            <a
+              href=" https://www.linkedin.com/in/edwin-diaz-663b93194/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="ls-buttons">LinkedIn</button>
+            </a>
+            <a
+              href="https://github.com/EEDS90"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="gh-buttons">GitHub</button>
+            </a>
+          </div>
             </div>
         </>
     )
